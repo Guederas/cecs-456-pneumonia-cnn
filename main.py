df853659abdbe56a3f953e6abd6fb874f0869754
Guederas <jean_guedes_sa@hotmail.com>
2025-12-16 14:32:42 -0800

    added model activation and convolution 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # CNN model that predicts Pneumonia or Normal given X-ray dataset
 import tensorflow as tf
 import keras
+from keras import layers, models, callbacks
 import os
 
 # Configuration
@@ -11,7 +12,7 @@ TRAIN_DIR = os.path.join('data', 'train')
 TEST_DIR = os.path.join('data', 'test')
 VAL_DIR = os.path.join('data', 'val')
 
-# Data loading
+# Load the dataset
 print("Loading Data...")
 
 train_ds = keras.utils.image_dataset_from_directory(
@@ -37,3 +38,14 @@ validate_ds = keras.utils.image_dataset_from_directory(
     label_mode='binary'
     color_mode='grayscale'
 )
+
+# CNN Model
+model = models.Sequential() # initializing the CNN
+
+model.add(layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1)))    # 1 channel for grayscale
+model.add(layers.Rescaling(1./255)) # normalizing pixel values
+
+# Convolution 1
+model.add(layers.Conv2D(32, (3, 3), strides=1, padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))
