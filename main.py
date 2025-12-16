2985dda8191e260d7f43ff694e2216e47a2f09ac
Guederas <jean_guedes_sa@hotmail.com>
2025-12-16 14:39:16 -0800

    added blocks with convolutions and pooling

--- main.py
+++ main.py
@@ -45,7 +45,30 @@ model = models.Sequential() # initializing the CNN
 model.add(layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1)))    # 1 channel for grayscale
 model.add(layers.Rescaling(1./255)) # normalizing pixel values
 
-# Convolution 1
-model.add(layers.Conv2D(32, (3, 3), strides=1, padding='same', activation='relu'))
+# Block 1
+model.add(layers.Conv2D(32, (3, 3), strides=1, padding='same', activation='relu'))  # convolution 1
 model.add(layers.BatchNormalization())
-model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))   # pooling 1
+
+# Block 2 (with Dropout)
+model.add(layers.Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))  # convolution 2
+model.add(layers.Dropout(0.1))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))   # pooling 2
+
+# Block 3
+model.add(layers.Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))  # convolution 3
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))   # pooling 3
+
+# Block 4 (with Dropout)
+model.add(layers.Conv2D(128, (3, 3), strides=1, padding='same', activation='relu'))  # convolution 4
+model.add(layers.Dropout(0.2))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))   # pooling 4
+
+# Block 5
+model.add(layers.Conv2D(256, (3, 3), strides=1, padding='same', activation='relu'))  # convolution 5
+model.add(layers.Dropout(0.2))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D((2, 2),strides=2, padding='same'))   # pooling 5
